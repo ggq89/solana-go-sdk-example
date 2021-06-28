c7a7deb938fc6559d457ee8ebd6cf8d46c377746
AlbertGou-asdx <guangquan.gou@ascendex.io>
2021-06-28 18:04:09 +0800

    upgrade new account

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/yihau/solana-go-sdk-example
 
 go 1.16
 
-require github.com/portto/solana-go-sdk v0.0.0-20210529143121-626264f07feb
+require (
+	github.com/cosmos/go-bip39 v1.0.0
+	github.com/mr-tron/base58 v1.2.0
+	github.com/portto/solana-go-sdk v0.0.0-20210529143121-626264f07feb
+)
--- tutorial/create-new-account/main.go
+++ tutorial/create-new-account/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	//"github.com/mr-tron/base58"
+	//"github.com/cosmos/go-bip39"
+	//"crypto/ed25519"
 
 	"github.com/portto/solana-go-sdk/types"
 )
 
 func main() {
+	//entropy, _ := bip39.NewEntropy(256)
+	//mnemonic, _ := bip39.NewMnemonic(entropy)
+	//seed := bip39.NewSeed(mnemonic, "")
+	//pri := ed25519.NewKeyFromSeed(seed[:32])
+	//pub := base58.Encode(pri.Public().(ed25519.PublicKey))
+
 	newAccount := types.NewAccount()
 	fmt.Println(newAccount.PublicKey.ToBase58())
 	fmt.Println(newAccount.PrivateKey)
