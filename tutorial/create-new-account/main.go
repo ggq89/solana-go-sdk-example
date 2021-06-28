ef1b328874dfb81d24835b9c08e262f7235721d3
AlbertGou-asdx <guangquan.gou@ascendex.io>
2021-06-28 19:26:44 +0800

    update transfer and create address

--- tutorial/create-new-account/main.go
+++ tutorial/create-new-account/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-	//"github.com/mr-tron/base58"
-	//"github.com/cosmos/go-bip39"
-	//"crypto/ed25519"
+	"github.com/cosmos/go-bip39"
+	"golang.org/x/crypto/ed25519"
 
 	"github.com/portto/solana-go-sdk/types"
 )
 
 func main() {
-	//entropy, _ := bip39.NewEntropy(256)
-	//mnemonic, _ := bip39.NewMnemonic(entropy)
-	//seed := bip39.NewSeed(mnemonic, "")
-	//pri := ed25519.NewKeyFromSeed(seed[:32])
-	//pub := base58.Encode(pri.Public().(ed25519.PublicKey))
-
 	newAccount := types.NewAccount()
 	fmt.Println(newAccount.PublicKey.ToBase58())
 	fmt.Println(newAccount.PrivateKey)
 
-	newAccount2 := types.AccountFromPrivateKeyBytes(newAccount.PrivateKey)
+	entropy, _ := bip39.NewEntropy(256)
+	mnemonic, _ := bip39.NewMnemonic(entropy)
+	fmt.Println(mnemonic)
+	seed := bip39.NewSeed(mnemonic, "")
+	pri := ed25519.NewKeyFromSeed(seed[:32])
+	//newAccount2 := types.AccountFromPrivateKeyBytes(newAccount.PrivateKey)
+	newAccount2 := types.AccountFromPrivateKeyBytes(pri)
 	fmt.Println(newAccount2.PublicKey.ToBase58())
 }
--- tutorial/transfer/main.go
+++ tutorial/transfer/main.go
@@ -8,29 +8,42 @@ import (
 	"github.com/portto/solana-go-sdk/common"
 	"github.com/portto/solana-go-sdk/sysprog"
 	"github.com/portto/solana-go-sdk/types"
+
+	"github.com/cosmos/go-bip39"
+	"golang.org/x/crypto/ed25519"
 )
 
 func main() {
-	c := client.NewClient("http://localhost:8899")
+	c := client.NewClient("https://api.devnet.solana.com")
 
-	feePayer := types.AccountFromPrivateKeyBytes([]byte{209, 25, 205, 128, 167, 154, 29, 44, 118, 161, 221, 56, 195, 253, 106, 150, 177, 135, 44, 222, 168, 0, 34, 39, 135, 108, 78, 107, 243, 139, 41, 55, 236, 166, 114, 58, 45, 235, 17, 246, 124, 48, 45, 88, 100, 215, 166, 123, 17, 131, 143, 26, 155, 72, 7, 203, 91, 245, 125, 83, 153, 238, 50, 186})
+	//feePayer := types.AccountFromPrivateKeyBytes([]byte{209, 25, 205, 128, 167, 154, 29, 44, 118, 161, 221, 56, 195, 253, 106, 150, 177, 135, 44, 222, 168, 0, 34, 39, 135, 108, 78, 107, 243, 139, 41, 55, 236, 166, 114, 58, 45, 235, 17, 246, 124, 48, 45, 88, 100, 215, 166, 123, 17, 131, 143, 26, 155, 72, 7, 203, 91, 245, 125, 83, 153, 238, 50, 186})
 
-	alice := types.AccountFromPrivateKeyBytes([]byte{196, 114, 86, 165, 59, 177, 63, 87, 43, 10, 176, 101, 225, 42, 129, 158, 167, 43, 81, 214, 254, 28, 196, 158, 159, 64, 55, 123, 48, 211, 78, 166, 127, 96, 107, 250, 152, 133, 208, 224, 73, 251, 113, 151, 128, 139, 86, 80, 101, 70, 138, 50, 141, 153, 218, 110, 56, 39, 122, 181, 120, 55, 86, 185})
+	//alice := types.AccountFromPrivateKeyBytes([]byte{196, 114, 86, 165, 59, 177, 63, 87, 43, 10, 176, 101, 225, 42, 129, 158, 167, 43, 81, 214, 254, 28, 196, 158, 159, 64, 55, 123, 48, 211, 78, 166, 127, 96, 107, 250, 152, 133, 208, 224, 73, 251, 113, 151, 128, 139, 86, 80, 101, 70, 138, 50, 141, 153, 218, 110, 56, 39, 122, 181, 120, 55, 86, 185})
 
 	res, err := c.GetRecentBlockhash(context.Background())
 	if err != nil {
 		log.Fatalf("get recent block hash error, err: %v\n", err)
 	}
+
+	seed := bip39.NewSeed("mnemonic", "")
+	pri := ed25519.NewKeyFromSeed(seed[:32])
+
 	rawTx, err := types.CreateRawTransaction(types.CreateRawTransactionParam{
 		Instructions: []types.Instruction{
 			sysprog.Transfer(
-				alice.PublicKey, // from
-				common.PublicKeyFromString("2xNweLHLqrbx4zo1waDvgWJHgsUpPj8Y8icbAFeR4a8i"), // to
-				1e9, // 1 SOL
+				//alice.PublicKey, // from
+				common.PublicKeyFromString("83R5RVHMEEmHtj9QydfAX958JDoNHKREmQhw8k24ryMj"),
+				common.PublicKeyFromString("9CfNE5H21Rqh1Wev28vZ3f41315GU6uz8khxt6ihh9Gs"), // to
+				5e8, // 1 SOL
 			),
 		},
-		Signers:         []types.Account{feePayer, alice},
-		FeePayer:        feePayer.PublicKey,
+		//Signers:         []types.Account{feePayer, alice},
+		Signers:         []types.Account{
+			//types.AccountFromPrivateKeyBytes([]byte{}),
+			types.AccountFromPrivateKeyBytes(pri),
+		},
+		//FeePayer:        feePayer.PublicKey,
+		FeePayer:common.PublicKeyFromString("83R5RVHMEEmHtj9QydfAX958JDoNHKREmQhw8k24ryMj"),
 		RecentBlockHash: res.Blockhash,
 	})
 	if err != nil {
