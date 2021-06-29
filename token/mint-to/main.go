cd675cdf443d61c13345d60edfe5d1730bd41345
AlbertGou-asdx <guangquan.gou@ascendex.io>
2021-06-29 18:03:22 +0800

    mint to spl

--- token/mint-to/main.go
+++ token/mint-to/main.go
@@ -10,18 +10,20 @@ import (
 	"github.com/portto/solana-go-sdk/types"
 )
 
-var feePayer = types.AccountFromPrivateKeyBytes([]byte{209, 25, 205, 128, 167, 154, 29, 44, 118, 161, 221, 56, 195, 253, 106, 150, 177, 135, 44, 222, 168, 0, 34, 39, 135, 108, 78, 107, 243, 139, 41, 55, 236, 166, 114, 58, 45, 235, 17, 246, 124, 48, 45, 88, 100, 215, 166, 123, 17, 131, 143, 26, 155, 72, 7, 203, 91, 245, 125, 83, 153, 238, 50, 186})
+//var feePayer = types.AccountFromPrivateKeyBytes([]byte{209, 25, 205, 128, 167, 154, 29, 44, 118, 161, 221, 56, 195, 253, 106, 150, 177, 135, 44, 222, 168, 0, 34, 39, 135, 108, 78, 107, 243, 139, 41, 55, 236, 166, 114, 58, 45, 235, 17, 246, 124, 48, 45, 88, 100, 215, 166, 123, 17, 131, 143, 26, 155, 72, 7, 203, 91, 245, 125, 83, 153, 238, 50, 186})
+var feePayer = types.AccountFromPrivateKeyBytes([]byte{})
 
-var alice = types.AccountFromPrivateKeyBytes([]byte{196, 114, 86, 165, 59, 177, 63, 87, 43, 10, 176, 101, 225, 42, 129, 158, 167, 43, 81, 214, 254, 28, 196, 158, 159, 64, 55, 123, 48, 211, 78, 166, 127, 96, 107, 250, 152, 133, 208, 224, 73, 251, 113, 151, 128, 139, 86, 80, 101, 70, 138, 50, 141, 153, 218, 110, 56, 39, 122, 181, 120, 55, 86, 185})
+//var alice = types.AccountFromPrivateKeyBytes([]byte{196, 114, 86, 165, 59, 177, 63, 87, 43, 10, 176, 101, 225, 42, 129, 158, 167, 43, 81, 214, 254, 28, 196, 158, 159, 64, 55, 123, 48, 211, 78, 166, 127, 96, 107, 250, 152, 133, 208, 224, 73, 251, 113, 151, 128, 139, 86, 80, 101, 70, 138, 50, 141, 153, 218, 110, 56, 39, 122, 181, 120, 55, 86, 185})
+var alice = types.AccountFromPrivateKeyBytes([]byte{})
 
-var mintPubkey = common.PublicKeyFromString("8TiyrubcVuSLPSrv5cuQnYBJE9fNNNJBBicaWYAWQif5")
+var mintPubkey = common.PublicKeyFromString("9WKBr1Gzt6YS1g4XMGHxTwZsmpyi5DHCyxqi7GuyNysC")
 
-var aliceTokenRandomTokenPubkey = common.PublicKeyFromString("HMBhVPJBEn8eqmCpmXDgpT7ywB5M7ZoS5WrcsNkBtNuW")
+//var aliceTokenRandomTokenPubkey = common.PublicKeyFromString("8928xaWivXEPQm5wMAjtkWnGCEocEvyiAPnKEBpHdQPX")
 
-var aliceTokenATAPubkey = common.PublicKeyFromString("51nAzsy57abkoBPguJzpkWSiRKvMECZxpmQUBM6qhVnm")
+var feeTokenATAPubkey = common.PublicKeyFromString("AKpvREJ4JpdtHYWuk4W5J2Sro1ZfN11q7kuN7omGRCAC")
 
 func main() {
-	c := client.NewClient("http://localhost:8899")
+	c := client.NewClient("https://api.devnet.solana.com")
 
 	res, err := c.GetRecentBlockhash(context.Background())
 	if err != nil {
@@ -31,15 +33,18 @@ func main() {
 		Instructions: []types.Instruction{
 			tokenprog.MintToChecked(
 				mintPubkey,
-				aliceTokenRandomTokenPubkey,
+				//aliceTokenRandomTokenPubkey,
+				feeTokenATAPubkey,
 				alice.PublicKey,
 				[]common.PublicKey{},
-				1e8,
-				8,
+				11e8,
+				9,
 			),
 		},
 		Signers:         []types.Account{feePayer, alice},
-		FeePayer:        feePayer.PublicKey,
+		//Signers:         []types.Account{alice},
+		//FeePayer:        feePayer.PublicKey,
+		FeePayer:        alice.PublicKey,
 		RecentBlockHash: res.Blockhash,
 	})
 	if err != nil {
